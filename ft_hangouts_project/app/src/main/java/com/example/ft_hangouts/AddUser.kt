5eac89c2fd35280b8c749b6b93bf22df16e912f2
gbouwen <gijsbouwen@gmail.com>
2021-05-30 16:54:19 +0200

    created addUser activity

---
+++ ft_hangouts_project/app/src/main/java/com/example/ft_hangouts/AddUser.kt
@@ -0,0 +1,12 @@
+package com.example.ft_hangouts
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_user)
+
+    }
+}
--- ft_hangouts_project/app/src/main/java/com/example/ft_hangouts/MainActivity.kt
+++ ft_hangouts_project/app/src/main/java/com/example/ft_hangouts/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.ft_hangouts
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addButton: Button = findViewById(R.id.add_button)
+        addButton.setOnClickListener {
+            val intent = Intent(this, AddUser::class.java)
+            startActivity(intent)
+        }
     }
 }
